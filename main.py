b87097263fc565c46e0e585d94c8cfcd29f3c7aa
Jesus Alberto Correa Morales <jesus_chivas_5000@hotmail.com>
2019-08-29 10:11:06 -0500

    Correcion de error.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
-import matplotlib as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-mnist = tf.keras.datasets.mnist
+mnist = tf.keras.datasets.mnist #28x28 images of handwriten 1-9
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
@@ -14,21 +15,19 @@ model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+
 model.fit(x_train, y_train, epochs=3)
 
 val_loss, val_acc = model.evaluate(x_test, y_test)
-print(val_loss, val_acc)
 
-model.save('epic_num_reader.model')
-new_model = tf.keras.models.load_model('epic_num_reader.model')
+predictions = model.predict([x_test])
 
-predictions = new_model.predict([x_test])
-print(predictions)
+print(np.argmax(predictions[0]))
 
-
-
-print(np.argmax(predictions[10]))
-
-plt.imshow(x_test[10])
+plt.imshow(x_test[0])
 plt.show()
+
+model.save('num_reader.model')
