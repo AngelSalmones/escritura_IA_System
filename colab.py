f81f71467cc69d59776feb887102d4f2ef9a6e10
Jesus Alberto Correa Morales <jesus_chivas_5000@hotmail.com>
2019-10-16 10:05:31 -0500

    Update

---
+++ colab.py
@@ -0,0 +1,120 @@
+# Imports
+from emnist import extract_training_samples, extract_test_samples
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import keras
+import os.path as path
+
+# Variables
+batch_size = 128
+num_classes = 62
+epochs = 5
+
+# Input image dimensions
+img_rows, img_cols = 28, 28
+
+# The data, split between train and test sets
+images_digits_training, labels_digits_training = extract_training_samples('digits')
+images_digits_test, labels_digits_test = extract_test_samples('digits')
+
+images_letters_training, labels_letters_training = extract_training_samples('letters')
+images_letters_test, labels_letters_test = extract_test_samples('letters')
+
+images_training = np.concatenate((images_digits_training, images_letters_training))
+images_test = np.concatenate((images_digits_test, images_letters_test))
+labels_training = np.concatenate((labels_digits_training, labels_letters_training))
+labels_test = np.concatenate((labels_digits_test, labels_letters_test))
+
+
+# Reshaping images acording to the format in keras
+if K.image_data_format() == 'channels_first':
+    images_training = images_training.reshape(images_training.shape[0], 1, img_rows, img_cols)
+    images_test = images_test.reshape(images_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    images_training = images_training.reshape(images_training.shape[0], img_rows, img_cols, 1)
+    images_test = images_test.reshape(images_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+images_training = images_training.astype('float32')
+images_test = images_test.astype('float32')
+images_training /= 255
+images_test /= 255
+print('images_training shape:', images_training.shape)
+print(images_training.shape[0], 'train samples')
+print(images_test.shape[0], 'test samples')
+
+# Convert class vectors to binary class matrices
+labels_training = keras.utils.to_categorical(labels_training, num_classes)
+labels_test = keras.utils.to_categorical(labels_test, num_classes)
+
+if not path.isfile('siquiero.model'):
+
+
+    # Creating the model
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(Conv2D(64, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    #Training the model
+    model.fit(images_training, labels_training,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(images_test, labels_test))
+
+    model.save('siquiero.model')
+
+
+else:
+
+    model = tf.keras.models.load_model('siquiero.model')
+
+# Model Score
+# val_loss, val_acc = model.evaluate(images_test, labels_test)
+# predictions = model.predict([images_test])
+# print(np.argmax(predictions[1787]))
+# #plt.imshow(images_test[1787], cmap='binary')
+# #plt.show()
+
+
+#val_loss, val_acc = model.evaluate(images_test, labels_test)
+
+my_image = plt.imread('A.jpeg')
+
+from skimage.transform import resize
+from skimage import color, io
+#my_image = io.imread('A.jpeg')
+my_image_resized = resize(my_image, (28,28,1))
+img = my_image_resized.astype('float32')
+img /= 255
+
+
+
+#predictions = model.predict(np.array( [my_image_resized,] ))
+#print(np.argmax(predictions[0]))
+#plt.imshow(color.rgb2gray(images_test[0]), cmap='binary')
+#plt.imshow(np.squeeze(img), cmap='binary')
+plt.imshow(np.squeeze(my_image_resized), cmap='binary')
+plt.show()
+
+
+#print('Test loss:', val_loss)
+#print('Test accuracy:', val_acc)
+
--- prueba.py
+++ prueba.py
@@ -4,26 +4,29 @@ import numpy as np
 import tensorflow as tf
 import os.path as path
 
-if not path.isfile('Entrenado.model'):
 
-	images_digits_training, labels_digits_training = extract_training_samples('digits')
-	images_digits_test, labels_digits_test = extract_test_samples('digits')
+images_digits_training, labels_digits_training = extract_training_samples('digits')
+images_digits_test, labels_digits_test = extract_test_samples('digits')
 
-	images_letters_training, labels_letters_training = extract_training_samples('letters')
-	images_letters_test, labels_letters_test = extract_test_samples('letters')
+images_letters_training, labels_letters_training = extract_training_samples('letters')
+images_letters_test, labels_letters_test = extract_test_samples('letters')
 
-	images_training = np.concatenate((images_digits_training, images_letters_training))
-	images_test = np.concatenate((images_digits_test, images_letters_test))
-	labels_training = np.concatenate((labels_digits_training, labels_letters_training))
-	labels_test = np.concatenate((labels_digits_test, labels_letters_test))
+images_training = np.concatenate((images_digits_training, images_letters_training))
+images_test = np.concatenate((images_digits_test, images_letters_test))
+labels_training = np.concatenate((labels_digits_training, labels_letters_training))
+labels_test = np.concatenate((labels_digits_test, labels_letters_test))
 
+
+if not path.isfile('siquiero.model'):
 
 	images_training = tf.keras.utils.normalize(images_training, axis=1)
 	images_test = tf.keras.utils.normalize(images_test, axis=1)
 
 	model = tf.keras.models.Sequential()
 	model.add(tf.keras.layers.Flatten())
-	model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+	# model.add(tf.keras.layers.Dense(528, activation=tf.nn.relu))
+	# model.add(tf.keras.layers.Dense(528, activation=tf.nn.relu))
+	# model.add(tf.keras.layers.Dense(528, activation=tf.nn.relu))
 	model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 	model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 	model.add(tf.keras.layers.Dense(62, activation=tf.nn.softmax))
@@ -32,25 +35,24 @@ if not path.isfile('Entrenado.model'):
 	             loss='sparse_categorical_crossentropy',
 	             metrics=['accuracy'])
 
-	model.fit(images_training, labels_training, epochs=3)
+	model.fit(images_training, labels_training, epochs=5)
 	
-	model.save('Entrenado.model')
+	model.save('Entrenado1prueba.model')
 
 else:
+	model = tf.keras.models.load_model('siquiero.model')
 
-	model = tf.keras.models.load_model('Entrenado.model')
+val_loss, val_acc = model.evaluate(images_test, labels_test)
+predictions = model.predict('3.jpeg')
+print(np.argmax(predictions[0]))
+plt.imshow('3.jpeg', cmap='binary')
+plt.show()
 
-	val_loss, val_acc = model.evaluate(images_test, labels_test)
-
-
-	predictions = model.predict([images_test])
-
-	print(np.argmax(predictions[1787]))
-
-
-
-	plt.imshow(images_test[1787], cmap='binary')
-	plt.show()
+# val_loss, val_acc = model.evaluate(images_test, labels_test)
+# predictions = model.predict([images_test])
+# print(np.argmax(predictions[1787]))
+# plt.imshow(images_test[1787], cmap='binary')
+# plt.show()
 
 
 
